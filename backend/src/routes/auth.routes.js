1ba55c1fe9cbac61226837af23bb5a1f39c0bee9
Nelson2023-2024 <nelsonobuya19@gmail.com>
2025-03-16 09:03:50 +0300

    Signup fuctionality completed to insert user to the DB only

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,8 +1,54 @@
 import { Router } from "express";
+import bcrypt from "bcrypt";
+import User from "../models/User.model.js";
 
 const router = Router();
 
-router.post("/register", async (req, res) => {});
+router.post("/register", async (req, res) => {
+  try {
+    const { email, userName, password } = req.body;
+
+    if (!email || !username || !password)
+      return res.status(400).json({ message: "All fields are required" });
+
+    if (password.length < 6)
+      return res
+        .status(400)
+        .json({ message: "Password should be at least 6 characters long" });
+
+    if (username.length < 3)
+      return res
+        .status(400)
+        .json({ message: "Username should be at least 3 characters long" });
+
+    //check if user already exists email or userName
+    // const existingUser = await User.findOne({ $or: [{ email }, { userName }] });
+    const existingEmail = await User.findOne({ email });
+    if (existingEmail)
+      return res.status(400).json({ message: "Email already exists" });
+
+    const existingUsername = await User.findOne({ userName });
+    if (existingUsername)
+      return res.status(400).json({ message: "Username already taken" });
+
+    //get a random avartar
+    const profileImage = `https://api.dicebear.com/9.x/avataaars/svg?seed=${userName}`;
+
+    const hashPass = await bcrypt.hash(password, 10);
+
+    //if the user doent exist create one
+    const user = new User({
+      email,
+      password: hashPass,
+      userName,
+      profileImage,
+    });
+    await user.save();
+  } catch (error) {
+    console.log("An Error occured in the Register route", error.message);
+    res.status(500).json({ error: error.message });
+  }
+});
 
 router.post("/login", async (req, res) => {});
 
