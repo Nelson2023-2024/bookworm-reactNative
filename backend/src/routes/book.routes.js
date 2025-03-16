05a39c5da739c5f482713551a54d08ede49e4608
Nelson2023-2024 <nelsonobuya19@gmail.com>
2025-03-17 03:45:36 +0530

    Create book route functionality completed

--- backend/src/routes/book.routes.js
+++ backend/src/routes/book.routes.js
@@ -5,7 +5,36 @@ import { protectRoute } from "../../middleware/protectRoute.middleware.js";
 
 const router = Router();
 
-router.use(protectRoute);
-router.post("/", async (req, res) => {});
+router.use(protectRoute)
+router.post("/", async (req, res) => {
+  try {
+    const { title, caption, rating, image } = req.body;
+
+    if (!image || !title || !caption || !rating)
+      res.status(400).json({ error: "All fields are required" });
+
+    //upload image to cloudinary
+    const uploadedResponse = await cloudinary.uploader.upload(image);
+
+    const imageURL = uploadedResponse.secure_url; //from the response get the image URL
+    //save to mongdb
+    const newBook = new Book({
+      title,
+      caption,
+      rating,
+      image,
+      user: req.user._id,
+    });
+
+    await newBook.save();
+
+    res.status(201).json(newBook);
+  } catch (error) {
+    console.log("An Error occured in the create-book route", error.message);
+    res
+      .status(500)
+      .json({ error: "Internal server Error", message: error.message });
+  }
+});
 
 export { router as bookRoutes };
