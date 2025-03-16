8c3e7221047dc37eebfda09829e1844e769ca7da
Nelson2023-2024 <nelsonobuya19@gmail.com>
2025-03-17 03:14:27 +0530

    Cloudinary setup

--- backend/src/routes/book.routes.js
+++ backend/src/routes/book.routes.js
@@ -1,7 +1,16 @@
 import { Router } from "express";
 
-const router = Router()
+const router = Router();
 
+router.post("/", async (req, res) => {
+    try {
+        
+    } catch (error) {
+        console.log("An Error occured in the create-book route", error.message);
+    res
+      .status(500)
+      .json({ error: "Internal server Error", message: error.message });
+    }
+});
 
-
-export {router as bookRoutes}
+export { router as bookRoutes };
