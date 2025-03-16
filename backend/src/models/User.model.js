b472f4bf2851f0a61641315d44d24c634b9de8f7
Nelson2023-2024 <nelsonobuya19@gmail.com>
2025-03-17 03:01:43 +0530

    Book model setup and basic book route setup

--- backend/src/models/User.model.js
+++ backend/src/models/User.model.js
@@ -1,27 +1,30 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  userName: {
-    type: String,
-    required: true,
-    unique: true,
+const userSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    profileImage: {
+      type: String,
+      default: "",
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 6,
-  },
-  profileImage: {
-    type: String,
-    default: "",
-  },
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 
-export default User
+export default User;
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import { configDotenv } from "dotenv";
 import { authRoutes } from "./routes/auth.routes.js";
 import { connectDB } from "./lib/db.js";
+import { bookRoutes } from "./routes/book.routes.js";
 configDotenv();
 
 const app = express();
@@ -9,6 +10,7 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use("/api/auth", authRoutes);
+app.use("/api/auth", bookRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
