1877c0abc3d9140b8eb276fcc2b59586b97ebd84
Nelson2023-2024 <nelsonobuya19@gmail.com>
2025-03-17 12:52:42 +0530

    Cors backend setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,16 +3,18 @@ import { configDotenv } from "dotenv";
 import { authRoutes } from "./routes/auth.routes.js";
 import { connectDB } from "./lib/db.js";
 import { bookRoutes } from "./routes/book.routes.js";
+import cors from "cors";
 configDotenv();
 
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cors())
 app.use("/api/auth", authRoutes);
 app.use("/api/book", bookRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
-  connectDB()
+  connectDB();
 });
